9ff0728b860796e605cb15700fcc12a6b427923c
KunalGhadge <103891060+KunalGhadge@users.noreply.github.com>
2026-05-06 11:59:49 +0530

    fix: remove unused _session to fix production build

--- src/app/(chat)/chat/page.tsx
+++ src/app/(chat)/chat/page.tsx
@@ -1,11 +1,9 @@
 import ChatBot from "@/components/chat-bot";
 import { generateUUID } from "lib/utils";
-import { getSession } from "auth/server";
 
 export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
-  const _session = await getSession();
   const id = generateUUID();
   return <ChatBot initialMessages={[]} threadId={id} key={id} />;
 }
